4960798f8edea3f4c5e73aaf04821c3039c404c6
GabriellyAssuncao <gabriellyassuncaorodrigues@gmail.com>
2024-12-02 16:07:09 -0300

    fix: adjusting configurations

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,12 +4,7 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails.
 rm -rf /app/tmp/pids/server.pid
 
-bundle exec rake db:drop
-bundle exec rake db:create
-
-# migrate
-bundle exec rake db:migrate
-
+bundle exec rake db:prepare
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
